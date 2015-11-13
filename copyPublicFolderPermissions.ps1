c96ab3670d260578597fcb61e5bda8514325b729
fakeSysAdmin <jimmy@tek-hut.com>
2015-11-12 17:48:30 -0700

    Parameter error handling
    Added parameter error handling. If user doesn't pass parameters, it lets
    them know.

--- copyPublicFolderPermissions.ps1
+++ copyPublicFolderPermissions.ps1
@@ -6,25 +6,37 @@
 
 #Get parameters from the user when they execute the script
 param([string]$user = "", [string]$folder = "", [string]$copyUser = "")
-"`n"
-
-#Attempt to be funny and informational
-Write-Host "Getting the public folder permissions from the user"$user" in the public folder "$folder"." -ForegroundColor Yellow
-Write-Host "Hold on to your butts!!!" -ForegroundColor Yellow
-
-#Grab public folder permissions from the user specified via the parameters
-Get-PublicFolder -Identity "\$folder" -Recurse | Get-PublicFolderClientPermission -user $user | Select-Object Identity, @{Name=’AccessRights’;Expression={[string]::join(";", ($_.AccessRights))}} | Export-Csv -Path C:\scripts\pubCopy.csv -NoTypeInformation
-
-#Read the permission information from the CSV file in C:\scripts\
-$pubFolderPerms = Import-CSV "C:\scripts\pubCopy.csv" | Select-Object `
-    @{name='ident';expression={$_.'Identity'}}, `
-    @{name='accessRight';expression={$_.'AccessRights'}}
-"`n"
-Write-Host "Grabbing permission info from the CSV file created and creating new public folder permissions for $copyUser..." -ForegroundColor Yellow
-
-#Write the permissions for the user specified in $copyUser
-foreach ($pubFolderPerm in $pubFolderPerms) {
-    Add-PublicFolderClientPermission -identity $pubFolderPerm.ident -User $copyUser -AccessRights $pubFolderPerm.accessRight
+if ($user -eq "" -or $folder -eq "" -or $copyUser -eq "") {
+    Write-Warning "You're missing one or more parameters. Please configure parameters when applying the script."
 }
-"`n"
-Write-Host "All done. Go back to reading reddit."
+else
+{
+    "`n"
+    #Attempt to be funny and informational
+    Write-Host "Getting the public folder permissions from the user"$user" in the public folder "$folder"." -ForegroundColor Yellow
+    Write-Host "Hold on to your butts!!!" -ForegroundColor Yellow
+    
+    #Grab public folder permissions from the user specified via the parameters
+    Get-PublicFolder -Identity "\$folder" -Recurse -ErrorAction Stop | Get-PublicFolderClientPermission -user $user -ErrorAction Stop | Select-Object Identity, @{Name=’AccessRights’;Expression={[string]::join(";", ($_.AccessRights))}} | Export-Csv -Path C:\scripts\pubCopy.csv -NoTypeInformation
+    
+    Write-Host "Something went horribly wrong. The Exchange server no longer works. You fail." -ForegroundColor Red
+    Break
+    
+    #Read the permission information from the CSV file in C:\scripts\
+    $pubFolderPerms = Import-CSV "C:\scripts\pubCopy.csv" | Select-Object `
+        @{name='ident';expression={$_.'Identity'}}, `
+        @{name='accessRight';expression={$_.'AccessRights'}}
+    "`n"
+    Write-Host "Grabbing permission info from the CSV file created and creating new public folder permissions for $copyUser..." -ForegroundColor Yellow
+
+    #Write the permissions for the user specified in $copyUser
+    foreach ($pubFolderPerm in $pubFolderPerms) {
+        Add-PublicFolderClientPermission -identity $pubFolderPerm.ident -User $copyUser -AccessRights $pubFolderPerm.accessRight
+        #Write-Host $pubFolderPerm.ident
+        #Write-Host $copyUser
+        #Write-Host $pubFolderPerm.accessRight
+    }
+    "`n"
+    Write-Host "All done. Go back to reading reddit." -ForegroundColor Blue -BackgroundColor White
+ 
+}
